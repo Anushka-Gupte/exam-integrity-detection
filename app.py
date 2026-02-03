de86e1db11f4d3357952ebb8d7af322fcce3f58f
Anushka-Gupte <akshatagupte393@gmail.com>
2026-02-03 18:46:43 +0530

    Real Dataset

--- app.py
+++ app.py
@@ -5,45 +5,36 @@ import pickle
 import shap
 import matplotlib.pyplot as plt
 
-model = pickle.load(open('exam-integrity-detection.pkl','rb'))
+model = pickle.load(open('classifier.pkl','rb'))
 scaler = pickle.load(open('scaler.pkl','rb'))
+transformer = pickle.load(open('transformer.pkl','rb'))
+imputer = pickle.load(open('imputer.pickle','rb'))
 
 st.header('Online Exam Cheating Detection System')
 
-st.subheader('Global Feature Importance')
-
-from sklearn.model_selection import train_test_split
-dataset = pd.read_csv('./data/online_exam_cheating.csv')
-X = dataset.iloc[:,:-1]
-y = dataset.iloc[:,-1].values
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=0)
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-explainer = shap.Explainer(model,X_train)
-shap_values = explainer(X_train)
-fig, ax = plt.subplots()
-shap.plots.bar(shap_values[:,:,1],max_display=10,show=False)
-st.pyplot(fig)
-st.write("""
-        This plot shows which features strongly influence the model's prediction overall for predicting cheating.
-         Larger mean SHAP values indicate features like Answer change rate and fast answer ratio are the most important drivers
-         influencing prediction of Cheating in an online exam. 
-        """)
 
 st.subheader('Input Features')
-avg = st.number_input('Average seconds spent per question',value=10.0,step=0.1)
-acc = st.number_input('Overall exam accuracy',value=0.1,step=0.1)
-fastans = st.number_input('Questions answered too fast',value=0.1,step=0.1)
-changeans = st.number_input('Average answer changes per question',value=0,step=1)
+uploaded_file = st.file_uploader('Enter co-ordinates of images',type='csv')
+if uploaded_file is not None:
+   test = pd.read_csv(uploaded_file,header=None)
+   test[[21,30]] = imputer.transform(test[[21,30]])
+   test = np.array(transformer.transform(test))
+   test[:,[11,12,13,14,15,16,17,18,19,20,21,22,
+           23,38,39,40,41,42,43]] = scaler.transform(test[:,[11,12,13,14,15,16,17,18,19,20,21,22,
+                                                          23,38,39,40,41,42,43]])
 
 st.subheader('Prediction Result')
 
 if st.button('Predict'):
-    X_inp = np.array([[avg,acc,fastans,changeans]])
-    X_inp[:,:1] = scaler.transform(X_inp[:,:1])
-    res = model.predict(X_inp)
-    if res == 0:
-       st.subheader('No cheating')
-    else:
-       st.subheader('Cheating')
+    res = model.predict(test)
+    i = 0
+    for r in res:
+      if r == 0:
+         st.markdown(f'### Student {i} No cheating')
+         i += 1
+         st.divider()
+      else:
+         st.markdown(f'### Student {i} Cheating')
+         i += 1
+         st.divider()
 
